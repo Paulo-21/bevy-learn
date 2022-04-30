55f62e76b99f760667c5a86418e0cee06421bcd7
paul.cibier <paul.cibier@gmail.com>
2022-04-30 17:57:08 +0200

    game

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,51 @@
 use bevy::prelude::*;
+use bevy::window::WindowMode;
 #[derive(Component)]
 struct Person;
+struct GreetTimer(Timer);
 
 #[derive(Component)]
 struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
-    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
-    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
-}
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
-    }
-}
-fn hello_world() {
-    println!("hello world!");
+fn setup(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        // Plane
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 8.0 })),
+            material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
+            transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
+            ..Default::default()
+        })
+        // Camera
+        .spawn(Camera3dBundle {
+            transform: Transform::from_matrix(Mat4::from_rotation_translation(
+                Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+                Vec3::new(-7.0, 20.0, 4.0),
+            )),
+            ..Default::default()
+        })
+        // Light
+        .spawn(LightBundle {
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            ..Default::default()
+        });
 }
 fn main() {
     println!("Bienvenue sur mon jeu");
-    App::new()
-        .add_startup_system(add_people)
-        .add_system(hello_world)
-        .add_system(greet_people)
+    
+        App::new()
+        // Set antialiasing to use 4 samples
+        .insert_resource(Msaa { samples: 4 })
+        // Set WindowDescriptor Resource to change title and size
+        .insert_resource(WindowDescriptor {
+            title: "Chess!".to_string(),
+            
+            mode : WindowMode::Fullscreen,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
         .run();
 }
