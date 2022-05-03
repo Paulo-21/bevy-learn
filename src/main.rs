a02770a2bcf5c00d9eb59394eb963baf5ed776bf
Paulo <paul.cibier@gmail.com>
2022-05-03 22:52:49 +0200

    game

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,58 @@
-use bevy::prelude::*;
+use std::ascii::AsciiExt;
+
+use bevy::{prelude::*, asset};
+use bevy::render::render_resource::TextureAspect;
+use bevy::window;
 use bevy::window::WindowMode;
-#[derive(Component)]
-struct Person;
-struct GreetTimer(Timer);
+
 
 #[derive(Component)]
-struct Name(String);
-fn setup(
-    commands: &mut Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    commands
-        // Plane
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Plane { size: 8.0 })),
-            material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
-            transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
-            ..Default::default()
-        })
-        // Camera
-        .spawn(Camera3dBundle {
-            transform: Transform::from_matrix(Mat4::from_rotation_translation(
-                Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
-                Vec3::new(-7.0, 20.0, 4.0),
-            )),
-            ..Default::default()
-        })
-        // Light
-        .spawn(LightBundle {
-            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
-            ..Default::default()
-        });
+struct Player;
+
+fn spawn_camera (mut commands : Commands) {
+   let mut camera_bundle = OrthographicCameraBundle::new_2d();
+    camera_bundle.orthographic_projection.scale = 1. / 50.;
+    commands.spawn_bundle(camera_bundle);
 }
+
+fn move_player(keys: Res<Input<KeyCode>>, mut player_query: Query<&mut Transform, With<Player>>) {
+    let mut direction = Vec2::ZERO;
+    if keys.any_pressed([KeyCode::Up, KeyCode::W]) {
+        direction.y += 1.;
+    }
+    if keys.any_pressed([KeyCode::Down, KeyCode::S]) {
+        direction.y -= 1.;
+    }
+    if keys.any_pressed([KeyCode::Right, KeyCode::D]) {
+        direction.x += 1.;
+    }
+    if keys.any_pressed([KeyCode::Left, KeyCode::A]) {
+        direction.x -= 1.;
+    }
+    if direction == Vec2::ZERO {
+        return;
+    }
+
+    let move_speed = 0.13;
+    let move_delta = (direction * move_speed).extend(0.);
+
+    for mut transform in player_query.iter_mut() {
+        transform.translation += move_delta;
+    }
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0., 0.47, 1.),
+            custom_size: Some(Vec2::new(1., 1.)),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+    .insert(Player);
+}
+
 fn main() {
     println!("Bienvenue sur mon jeu");
     
@@ -41,11 +61,15 @@ fn main() {
         .insert_resource(Msaa { samples: 4 })
         // Set WindowDescriptor Resource to change title and size
         .insert_resource(WindowDescriptor {
-            title: "Chess!".to_string(),
-            
-            mode : WindowMode::Fullscreen,
+            title: "Jeux video".to_string(),
+            present_mode : window::PresentMode::Fifo,
+            mode : WindowMode::BorderlessFullscreen,
             ..Default::default()
         })
+        .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53)))
         .add_plugins(DefaultPlugins)
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_player)
+        .add_system(move_player)
         .run();
 }
