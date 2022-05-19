3399cff9f58b0d5d347e7380e83b77275d5524df
Paulo-21 <paul.cibier@gmail.com>
2022-05-19 21:00:04 +0200

    refactor camera_focus

--- src/camera.rs
+++ src/camera.rs
@@ -28,30 +28,36 @@ pub struct CameraInfo {
 pub fn camera_focus(
     mut query: ParamSet<(
         Query<&mut CameraInfo, With<Camera3d>>,
-        Query<&Transform, With<Player>>,
         Query<&mut Transform, With<Camera3d>>,
     )>,
+    player_query : Query<&Transform, With<Player>>,
     windows: Res<Windows>,
     mut mouse_motion_events: EventReader<MouseMotion>,
 ) {
     let window = windows.get_primary().unwrap();
     let mut yaw = 0.0;
     let mut pitch = 0.0;
-    for mut info in query.p0().iter_mut() {
-        for event in mouse_motion_events.iter() {
-            info.input_state.pitch -= (0.00012 * event.delta.y * window.height()).to_radians();
-            info.input_state.yaw -= (0.00012 * event.delta.x * window.width()).to_radians();
-        }
-        pitch = info.input_state.pitch;
-        yaw = info.input_state.yaw;
+    let mut info_query = query.p0();
+    let mut info = info_query.get_single_mut().unwrap();
+    for event in mouse_motion_events.iter() {
+        info.input_state.pitch -= (0.00012 * event.delta.y * window.height()).to_radians();
+        info.input_state.yaw -= (0.00012 * event.delta.x * window.width()).to_radians();
     }
-    let camera_pos = {
-        let player_query = query.p1();
-        let player_transform = player_query.get_single().unwrap();
-        player_transform.translation + player_transform.rotation.mul_vec3(Vec3::new(0.0, 4.5, -6.0))
+    pitch = info.input_state.pitch;
+    yaw = info.input_state.yaw;
+    
+    let player_transform = player_query.get_single().unwrap();
+    let camera_pos: Vec3 = match info.camera_mode {
+        CameraMode::Fps => {
+            player_transform.translation + player_transform.rotation.mul_vec3(Vec3::new(0.0, 4.5, -6.0))
+        },
+        CameraMode::Top => {
+            player_transform.translation + player_transform.rotation.mul_vec3(Vec3::new(0.0, 4.5, -6.0))
+        }
     };
+    
 
-    for mut transform in query.p2().iter_mut() {
+    for mut transform in query.p1().iter_mut() {
         transform.translation = Vec3::new(camera_pos.x, camera_pos.y, camera_pos.z);
         transform.rotation = Quat::from_axis_angle(Vec3::Y, yaw)
         * Quat::from_axis_angle(Vec3::X, pitch);
